04e9d158158dd941baa205f7f572e72afc5e49e0
Sranjan4321 <shashi94ranjan@gmail.com>
2022-05-25 18:38:27 +0530

    commit

--- advance/src/assign/Hooks/Check.tsx
+++ advance/src/assign/Hooks/Check.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import useTimeout from './useTimeOut'
-import useTimer from './useTimer'
-const Check = () => {
-    const {time,start,stop,reset}=useTimer(5)
-    const {times}=useTimeout(4)
-  return (
-      <>
-      <div>
-        <h1>Use Time Custom Hooks</h1>
-        <h2>{time}</h2>
-       <button onClick={start}>start timer</button>
-       <button onClick={stop}>stop timer</button>
-       <button onClick={reset}>Reset timer</button>
-    </div>
-    <div>
-        <h1>{times?"time up":"wiat"}</h1>
-    </div>
-      </>
+// import React from 'react'
+// import useTimeout from './useTimeOut'
+// import useTimer from './useTimer'
+// const Check = () => {
+//     const {time,start,stop,reset}=useTimer(5)
+//     const {times}=useTimeout(4)
+//   return (
+//       <>
+//       <div>
+//         <h1>Use Time Custom Hooks</h1>
+//         <h2>{time}</h2>
+//        <button onClick={start}>start timer</button>
+//        <button onClick={stop}>stop timer</button>
+//        <button onClick={reset}>Reset timer</button>
+//     </div>
+//     <div>
+//         <h1>{times?"time up":"wiat"}</h1>
+//     </div>
+//       </>
     
-  )
-}
+//   )
+// }
 
-export default Check
+// export default Check
+import React from "react";
