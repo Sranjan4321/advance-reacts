cea748cd2bdb3bacc7e18009738469daed3964ec
Sranjan4321 <shashi94ranjan@gmail.com>
2022-05-25 18:38:44 +0530

    commit

--- advance/src/assign/Hooks/useTimer.ts
+++ advance/src/assign/Hooks/useTimer.ts
@@ -2,39 +2,41 @@ import React, { useRef, useState } from "react"
 
 
 
- const useTimer=(initTime:number)=>{
-     const [time,setTime]=useState(initTime)
-     const [flag,setFlag]=useState(false)
-     const timeref=useRef<number|undefined>(null)
-     const stop=()=>{
-        setFlag(true)
-        window.clearInterval(timeref.current)
-     //    console.log(flag)
+//  const useTimer=(initTime:number)=>{
+//      const [time,setTime]=useState(initTime)
+     
+//      const timeref=useRef<number|undefined>(null)
+//      const stop=()=>{
+        
+//         window.clearInterval(timeref.current)
+//      //    console.log(flag)
        
-      }
-      const reset=()=>{
-        window.clearInterval(timeref.current)
-       setTime(initTime)
-    }
-     const start=()=>{
-     timeref.current= window.setInterval (()=>{
-            setTime(state=>state-1)
+//       }
+//       const reset=()=>{
+//           if(timeref.current&&timeref){
+//             window.clearInterval(timeref.current)
+//           }
+       
+//        setTime(initTime)
+//     }
+//      const start=()=>{
+//      timeref.current= window.setInterval (()=>{
+//             setTime(state=>state-1)
 
-           },1000)
-           return ()=> {
-            stop()
-        }
-       }
+//            },1000)
+//            return ()=> {
+//             stop()
+//         }
+//        }
        
     
     
     
-return{
-    time,
-    start,
-    stop,
-    reset,
-
-}
-}
-export default useTimer
+// return{
+//     time,
+//     start,
+//     stop,
+//     reset,
+// }
+// }
+// export default useTimer
